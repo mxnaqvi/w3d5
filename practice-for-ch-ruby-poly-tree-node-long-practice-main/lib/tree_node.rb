7e59e750769b96a86a2ca47bf83596b0d4c8b681
Mohammad Naqvi <mohammadnaqvi25@gmail.com>
2023-03-24 15:44:06 -0400

    finished up to phase 4

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,40 @@
 class PolyTreeNode
+    attr_reader :value, :children, :parent
+  def initialize(value)
+    @parent = nil
+    @children =  []
+    @value = value
+  end
+
+#   def value 
+
+  def parent=(new_parent)
+
+    if @parent == nil
+        @parent = new_parent
+        @parent.children << self if !@parent.children.include?(self)
+    elsif 
+        old_child = @parent.children.delete(self)
+        @parent = new_parent
+        @parent.children << old_child if new_parent != nil
+    end
+  end
+
+  def add_child(child_node)
+    child_node.parent=self
+  end
   
+  def remove_child(child_node)
+    child_node.parent=nil
+  end
+
+#   def dfs(target)
+#     return self if self == target
+
+#         children.each do |child|
+#             # return child if dfs(child) != nil
+#         end
+#      return nil if self.parent == nil  
+#   end
+
 end
